96279a514ad7b573f7ea4a1e31fe46d4859f5bda
Skye Santilla <sesantilla@csuchico.edu>
2026-02-18 09:34:31 -0800

    Fixed Issue #3 - Code has notes to make work clear and understandable

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// This is a simple C++ program that prints "Hello, World!" to the console.
+main ()
+{
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate that the program ended successfully
+    return 0;
+}
